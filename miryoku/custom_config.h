2bd72e0296489e582c0dd46aac571b7eada99b44
John Bonini <bonini.physics@gmail.com>
2024-11-04 11:59:18 -0500

    space comma available in num layer (arrays)

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -36,5 +36,5 @@ U_NP,              U_NP,              &kp K_APP,         &kp SPACE,         &kp 
 #define MIRYOKU_LAYER_NUM \
 &kp LBKT,          &kp N7,            &kp N8,            &kp N9,            &kp RBKT,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
 &kp SEMI,          &kp N4,            &kp N5,            &kp N6,            &kp EQUAL,         U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
-&kp GRAVE,         &kp N1,            &kp N2,            &kp N3,            &kp BSLH,          U_NA,              &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          U_NA,              \
+&kp GRAVE,         &kp N1,            &kp N2,            &kp N3,            &kp BSLH,          U_NA,              &kp SPACE,       &kp COMMA,       &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp MINUS,         &kp LSHIFT,              U_NA,        &kp RET,              U_NP,              U_NP
